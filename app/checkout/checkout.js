5973d4d55852af3386fb2c5589596eef019c7cf8
amymc <amy.mccarthy2@mail.dcu.ie>
2017-01-19 19:17:29 +0000

    add christmas surcharge

--- app/checkout/checkout.js
+++ app/checkout/checkout.js
@@ -14,19 +14,20 @@ angular.module('myApp.checkout', ['ngRoute'])
 
   $scope.bouquetQuantity = 1;
   $scope.currentDate = new Date().toString();
+  $scope.totalPrice = 0.00
 
   $scope.shippingOptions = [
     {
-      type: 'Royal Mail (free)',
-      price: 0.00
+      type: "Royal Mail (free)",
+      price: "0.00"
     },
     {
-      type:'DPD Courier (£5.00)',
-      price: 5.00
+      type:"DPD Courier (£5.00)",
+      price: "5.00"
     },
     {
-      type: 'DPD Morning Courier (£6.00)',
-      price: 6.00
+      type: "DPD Morning Courier (£6.00)",
+      price: "6.00"
     }
   ];
 
@@ -34,10 +35,22 @@ angular.module('myApp.checkout', ['ngRoute'])
     $scope.bouquetCost = bouquetCost;
   };
 
+  $scope.checkDate  = function(deliveryDate) {
+    $scope.selectedDate = new Date(deliveryDate);
+    var month = $scope.selectedDate.getMonth();
+    var day = $scope.selectedDate.getDate();
+    var isChristmas = ((month === 0 && day < 3) || (month === 11 && day > 22));
+
+    $scope.surcharge = isChristmas ? 3.50 : 0;
+
+    console.log('$scope.surcharge', $scope.surcharge);
+    $scope.calculateTotal();
+  };
+
   $scope.calculateTotal = function() {
     console.log('$scope.bouquetQuantity', $scope.bouquetCost);
     if ($scope.deliveryDate && $scope.shippingCost && $scope.bouquetQuantity && $scope.bouquetCost) {
-      $scope.totalPrice = ($scope.bouquetCost * $scope.bouquetQuantity) + parseInt($scope.shippingCost);
+      $scope.totalPrice = ($scope.bouquetCost * $scope.bouquetQuantity) + parseInt($scope.shippingCost) + $scope.surcharge;
       console.log('$scope.totalPrice ', $scope.totalPrice );
     }
   };
